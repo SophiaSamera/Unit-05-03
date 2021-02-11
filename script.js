ffb02ce441fe073e4da839ede94bb29b1ded59c2
SophiaSamera <Sophia.samera@ocsbstudent.ca>
2021-02-11 15:24:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('display-message').addEventListener('click',buttonOn);
+
+function buttonOn() {
+  alert('Hello!')
+  document.getElementById('background').style.backgroundColor = "lightblue"
+  }
+  
