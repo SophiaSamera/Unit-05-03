be9ceebe03dd33986dffaf6bb8fc68578e79943a
SophiaSamera <Sophia.samera@ocsbstudent.ca>
2021-02-11 15:37:05 +0000

    Changed indentation and a spacebar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('display-message').addEventListener('click', buttonOn)
 
 function buttonOn () {
-alert('Hello!')
-document.getElementById('background') .style.backgroundColor = 'lightblue'
+  alert('Hello!')
+  document.getElementById('background').style.backgroundColor = 'lightblue'
 }
